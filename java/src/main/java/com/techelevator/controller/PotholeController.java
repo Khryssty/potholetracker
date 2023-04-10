37949e5e22e866ae2df02cc6bfed9190d69734b7
Christy Grothues <cggumban@gmail.com>
2023-04-10 19:18:58 -0400

    PotholeController

--- java/src/main/java/com/techelevator/controller/PotholeController.java
+++ java/src/main/java/com/techelevator/controller/PotholeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -28,8 +29,8 @@ public class PotholeController {
 
     @PreAuthorize("permitAll")
     @RequestMapping(method = RequestMethod.GET)
-    public List<PotholeDto> findAll(){
-        return service.getAllPotholes();
+    public List<PotholeDto> findAll(Principal principal){
+        return service.getAllPotholes(principal);
         }
 
 
