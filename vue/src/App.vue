5420fadde0faee6c735fde222253a78f26125e70
Kim Mariano <kimberlee.mariano@gmail.com>
2023-04-11 13:22:40 -0400

    worked on app.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,15 +1,69 @@
-
-
-
 <template>
+
+<!-- Added -->
+<div class = "container"> 
+  <header>
+  Road Rough Repairs 
+
+  <!-- need to add logo and name -->
+  
+  </header>
+
+
+
+<!-- Provided in source code
   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+
     </div>
     <router-view />
+
+    -->
+
+    <!--TODO router link to PotholeList for Navigation Bar
+    route to login 
+
+    view potholes, register, login on navigation bar
+
+    -->
+    
+      
+          <nav>
+      <div class="content">
+        Navigation
+        <div><router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+        <div><router-link v-bind:to="{ name: 'login' }">Login</router-link>
+        <div><router-link v-bind:to="{ name: 'register'}">Register</router-link>
+
+
+
   </div>
-</template>
+  </nav>
+
+  <main>
+    <div class="content">
+
+      </div>
+
+      </main>
+      <aside>
+        <div class="content">
+          Sidebar / Info
+        </div>
+        </aside>
+        <footer>
+          <div class="content">
+            Footer content
+            </div>
+
+        </footer>
+        </div>
+
+        </template>
+
+  
 
 
 <!-- Need to add Holy Grail css -->
