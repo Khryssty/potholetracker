b341a1137083ec537fad3668729279b1528f8595
Christy Grothues <cggumban@gmail.com>
2023-04-11 16:51:13 -0400

    Corrections on the list vue

--- vue/src/components/PotholeList.vue
+++ vue/src/components/PotholeList.vue
@@ -1,25 +1,35 @@
 <template>
   <div class="pothole-list">
+      <div>
       <table>
             <thead>
                 <tr>
                     <th>POTHOLE ID</th>
                     <th>STATUS</th>
+                    <th>STATUS DATE</th>
                     <th>SEVERITY</th>
                     <th>ADDRESS</th>
-                    <th>DATE</th>
+                    <th>ASSIGNED TO</th>
+                    <th>PHOTO</th>                    
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="pothole in this.$store.state.potholes" v-bind:key="pothole.id">
-                    <td>{{pothole.id}}</td>
+                    <td>{{pothole.potholeId}}</td>
                     <td>{{pothole.status}}</td>
+                    <td>{{pothole.statusDate}}</td>
                     <td>{{pothole.severity}}</td>
                     <td>{{pothole.address}}</td>
-                    <td>{{pothole.date}}</td>
-                </tr>
+                    <td>{{pothole.username}}</td>
+                    <td>{{pothole.photo}}</td>
+                </tr>              
             </tbody>
       </table>
+      </div>
+      
+       <div>
+           TBD: Maybe display a map here with outstanding potholes to repair.
+       </div>
   </div>
 </template>
 
