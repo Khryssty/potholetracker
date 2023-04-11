ed9b8a8cbe4eb6e2f24790817277c05cf657db6b
Christy Grothues <cggumban@gmail.com>
2023-04-11 13:17:27 -0400

    Edited app.vue for holy grail format, added pothole view and upated index.js

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,16 +1,142 @@
-
-
-
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
-    <router-view />
+  <div class="container">
+    <header>
+      Rough Road Repairs
+    </header>
+
+    <nav>
+        <div class="content">          
+          <div><router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;</div>        
+          <div><router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link></div>    
+          <div><router-link v-bind:to="{ name: 'viewPotholes'}">View Potholes</router-link></div>
+        </div>
+
+    </nav>
+
+    <main>
+      <div class="content">
+          <router-view />
+          <!-- Maybe a map with TBD-->
+      </div>
+    </main>
+
+    <aside>
+      <div class="content">
+        Aside Navigation
+      </div>  
+    </aside>
+
+    <footer>
+      <div class="content">
+        Footer content
+      </div>
+    </footer>
+    
   </div>
 </template>
 
+<script>    
+   
+  export default {
+   
+  };
+</script>
 
 <!-- Need to add Holy Grail css -->
 
+<style>
+    body {
+        margin: 0;
+    }
+
+    /* Add CSS Grid here  background colors: Header - #f69a00;  Nav - #f2ead2;  
+    main - #f9f5f0;  aside - #f2ead2;  footer - #331312;
+    font colors header, footer - #ffffff;  nav, main, aside - #74889a;*/
+
+    .container{
+        display: grid;
+
+        grid-template-areas: 
+            "header header header" 
+            "nav content side"
+            "footer footer footer";
+
+        grid-template-columns: 200px 1fr 200px;
+        grid-template-rows: 125px 1fr 85px;
+        gap: 10px;
+
+        height: 100vh;
+
+
+    }
+    /*handle responsive behavior */
+    @media (max-width: 768px) {
+        .container{
+            grid-template-areas: 
+                "header"
+                "nav"
+                "content"
+                "side"
+                "footer";
+            grid-template-columns: 1fr;
+            grid-template-rows:
+                auto /*Header */
+                75px /*Nav*/
+                1fr /* content */
+                75px /* Sidebar */
+                auto; /* Footer */
+            
+        }
+        header, footer, nav, main, aside {
+            padding: 5px 0;
+        }
+        nav, aside {
+        margin: 0;
+        }
+
+    }
+
+    header {
+    grid-area: header;
+    background-color: #f69a00;
+    }
+
+    nav {
+        grid-area: nav;
+        margin-left: 0.5rem;
+        background-color: #f2ead2;
+    }
+
+    main {
+        grid-area: content;
+        background-color: #f9f5f0;
+    }
+
+    aside {
+        grid-area: side;
+        margin-right: 0.5rem;
+        background-color: #f2ead2;
+    }
+
+    footer {
+        grid-area: footer;
+        background-color: #331312;
+    }
+
+    header, footer {
+        color: #ffffff;
+    }
+    nav, main, aside {
+        color: #74889a;
+    }
+    header, footer, nav, main, aside {
+        font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-size: 24px;
+        text-transform: uppercase;
+    }
+
+    div.content {
+        text-align: center;
+        padding-top: 20px;
+    }
+</style>
