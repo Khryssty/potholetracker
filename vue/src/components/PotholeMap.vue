c5b9ae8e510cbf1ff2dffc0dac3e9cc9c1b329a2
Bradley Taylor <bradleyctaylor22@gmail.com>
2023-04-12 13:27:36 -0400

    Clicking map adds a marker and displays lat/long

--- vue/src/components/PotholeMap.vue
+++ vue/src/components/PotholeMap.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="container">
-      <div id="map"></div>
+    <div>
+      <p>Latitude: {{latitude}}</p>
+      <p>Longitude: {{longitude}}</p>
+    </div>
+    <div id="map"></div>
   </div>
 </template>
 
@@ -9,36 +13,58 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
 export default {
- name: "pothole-map",
- data() {
-   return{
-     center: [39.739071, -75.539787]
-   }},
+  name: "pothole-map",
+  data() {
+    return {
+      center: [39.739071, -75.539787],
+      icon: L.divIcon({
+        className: 'my-icon'
+      }),
+      latitude: '',
+      longitude: '',
+      layer: ''
+    };
+  },
   // props: ["lat", "lng"],
- methods: {
-   setupLeafletMap: function () {
-     const mapDiv = L.map("map").setView(this.center, 13);
-     L.tileLayer(
-       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-       {
-         attribution:
-           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-         maxZoom: 16,
-         id: "mapbox/streets-v11",
-         accessToken:'pk.eyJ1IjoiYnJhZGxleWN0YXlsb3IiLCJhIjoiY2xnZHJqMThhMDBvaTNybzdlNWQxb2M5ayJ9.ortNBF7OocvnnTGZxr063A',
-       }
-     ).addTo(mapDiv);
-   },
- },
- mounted() {
-   this.setupLeafletMap();
- },
+  methods: {
+    setupLeafletMap: function () {
+      const mapDiv = L.map("map").setView(this.center, 13);
+      L.tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+        {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          maxZoom: 32,
+          id: "mapbox/streets-v11",
+          accessToken:
+            "pk.eyJ1IjoiYnJhZGxleWN0YXlsb3IiLCJhIjoiY2xnZHJqMThhMDBvaTNybzdlNWQxb2M5ayJ9.ortNBF7OocvnnTGZxr063A",
+        }
+      ).addTo(mapDiv);
+      mapDiv.on("click", (event) => {
+        this.latitude = event.latlng.lat;
+        this.longitude = event.latlng.lng;
+        if(this.layer !== '') {
+          this.layer.remove();
+        }
+        this.layer = L.marker(event.latlng, {icon: this.icon, title: event.latlng}).addTo(mapDiv);
+      });
+    },
+  },
+  mounted() {
+    this.setupLeafletMap();
+  },
 };
 </script>
 
 <style>
-    #map {
-        width: 600px;
-        height: 600px;
-    }
+#map {
+  width: 600px;
+  height: 600px;
+}
+div.my-icon {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: red;
+}
 </style>
