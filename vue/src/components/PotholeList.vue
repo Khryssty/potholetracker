4d928414405af1c4512d4d44123358d768e52a3a
Christy Grothues <cggumban@gmail.com>
2023-04-14 14:07:25 -0400

    View functionality on the front-end. Admin have a view of all potholes while user and public cannot view deleted and repaired status

--- vue/src/components/PotholeList.vue
+++ vue/src/components/PotholeList.vue
@@ -14,62 +14,63 @@
           </tr>
         </thead>
         <tbody>
-          <tr
-            v-for="pothole in $store.state.potholes"
-            v-bind:key="pothole.potholeId"
-          >
-            <td>{{ pothole.potholeId }}
-              <button v-bind:key="pothole.potholeId" v-if = "hasChanges" v-on:click="saveChanges(pothole)">Save</button>  
-            </td>            
+          <template v-for="pothole in $store.state.potholes">             
+            <template  v-if="!(!currentUser.username || currentUser.authorities[0].name !== 'ROLE_ADMIN') ||
+                  !(pothole.status === 'deleted' || pothole.status === 'repaired')">
+              <tr v-bind:key="pothole.potholeId">
+                  <td>{{ pothole.potholeId }}
+                    <button v-bind:key="pothole.potholeId" v-if = "hasChanges" v-on:click="saveChanges(pothole)">Save</button>  
+                  </td>            
 
-          <template v-if="currentUser.username && currentUser.authorities[0].name === 'ROLE_ADMIN'">
-            
-            <td>
-              <select class="status" v-model="pothole.status" v-bind:key="pothole.status" @change="onStatusChange">
-                <option
-                  v-for="option in statusOptions"
-                  v-bind:key="option.value"
-                  v-bind:value="option.value"
-                  v-bind:selected="pothole.status === option.value"
-                >
-                  {{ option.label }}
-                </option>
-              </select>              
-            </td>                    
-            
-            <td>
-              <select class="severity" v-model="pothole.severity" v-bind:key="pothole.severity" @change="onSeverityChange">>
-                <option 
-                  v-for="option in severityOptions"
-                  v-bind:key="option.value"
-                  v-bind:value="option.value"
-                  v-bind:select="pothole.severity === option.value">
-                  {{option.label}}
-                </option>
-              </select>            
-            </td>                            
-         </template>   
+                  <template v-if="currentUser.username && currentUser.authorities[0].name === 'ROLE_ADMIN'">
+                    
+                    <td>
+                      <select class="status" v-model="pothole.status" v-bind:key="pothole.status" @change="onStatusChange">
+                        <option
+                          v-for="option in statusOptions"
+                          v-bind:key="option.value"
+                          v-bind:value="option.value"
+                          v-bind:selected="pothole.status === option.value"
+                        >
+                          {{ option.label }}
+                        </option>
+                      </select>              
+                    </td>                    
+                    
+                    <td>
+                      <select class="severity" v-model="pothole.severity" v-bind:key="pothole.severity" @change="onSeverityChange">>
+                        <option 
+                          v-for="option in severityOptions"
+                          v-bind:key="option.value"
+                          v-bind:value="option.value"
+                          v-bind:select="pothole.severity === option.value">
+                          {{option.label}}
+                        </option>
+                      </select>            
+                    </td>                            
+                </template>   
 
 
-         <template v-else>
-            <td>{{ pothole.status }}</td>    
-            <td>{{ pothole.severity }}</td>
-         </template>
+                <template v-else>
+                    <td>{{ pothole.status }}</td>
+                    <td>{{ pothole.severity }}</td>
+                </template>
 
-            <td>{{ pothole.statusDate }}</td>
-            <td>{{ pothole.username }}</td>
+                <td>{{ pothole.statusDate }}</td>
+                <td>{{ pothole.username }}</td>
 
-            <td v-if="pothole.location.street == ''">
-              {{ pothole.location.lat }}, {{ pothole.location.lng }}
-            </td>
-            <td v-else>
-              {{ pothole.location.street }}, {{ pothole.location.city }},
-              {{ pothole.location.state }}, {{ pothole.location.postalCode }}
-            </td>
-            
-            <td>{{ pothole.photo }}</td>
-            <!-- <td v-if ="hasChanges"><button v-on:click="saveChanges(pothole)">Save</button></td>     -->
-          </tr>
+                <td v-if="pothole.location.street == ''">
+                  {{ pothole.location.lat }}, {{ pothole.location.lng }}
+                </td>
+                <td v-else>
+                  {{ pothole.location.street }}, {{ pothole.location.city }},
+                  {{ pothole.location.state }}, {{ pothole.location.postalCode }}
+                </td>
+                
+                <td>{{ pothole.photo }}</td>              
+              </tr>
+            </template>
+          </template>          
         </tbody>
       </table>
     </div>
