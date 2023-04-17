d1cadad5cd88c5752d1aeadfb16717b4fe4fc129
abu rustam <arif17jahan@gmailc.om[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C.com[3~[3~[3~>
2023-04-17 10:47:54 -0400

    exception classes created

--- java/src/main/java/com/techelevator/controller/FileController.java
+++ java/src/main/java/com/techelevator/controller/FileController.java
@@ -1,5 +1,7 @@
 package com.techelevator.controller;
 
+import com.techelevator.model.UploadFileResponse;
+import com.techelevator.service.FileStorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,4 +61,4 @@ public class FileController {
                 .body(resource);
     }
 }
-}
+
---
+++ java/src/main/java/com/techelevator/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.techelevator.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
