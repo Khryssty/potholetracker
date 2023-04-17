9894a84a5f085b96aba07965f6bfe93633cce35b
Kim Mariano <kimberlee.mariano@gmail.com>
2023-04-17 10:05:14 -0400

    worked on image backend

--- java/src/main/java/com/techelevator/Application.java
+++ java/src/main/java/com/techelevator/Application.java
@@ -1,9 +1,15 @@
 package com.techelevator;
 
+import com.techelevator.model.FileStoragePropertiesDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStoragePropertiesDto.class
+})
+
 public class Application {
 
     public static void main(String[] args) {
