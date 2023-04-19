f95ca37d06099bc6cfff057dfe1896006e020b05
Christy Grothues <cggumban@gmail.com>
2023-04-19 13:09:11 -0400

    Styling on register view

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,13 +1,10 @@
 <template>
+  <div id="register" class="register-img">
+      <!-- <img src="img/registerpage2.png" alt="Register Page" />  -->
 
 
-
-  <div id="register" class="text-center">
-      <img src="img/registerpage2.png" alt="Register Page" /> 
-
-
-    <form @submit.prevent="register">
-      <h1>Create Account</h1>
+    <form @submit.prevent="register" class="register-form">
+      <h3>Create Account</h3>      
       <div role="alert" v-if="registrationErrors">
         {{ registrationErrorMsg }}
       </div>
@@ -24,7 +21,7 @@
         <input type="password" id="confirmPassword" v-model="user.confirmPassword" required />
       </div>
       <button type="submit">Create Account</button>
-      <p><router-link :to="{ name: 'login' }">Already have an account? Log in.</router-link></p>
+      <router-link :to="{ name: 'login' }">Login if you have an account</router-link>
     </form>
 
     
@@ -82,33 +79,101 @@ export default {
 </script>
 
 <style scoped>
-#register {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 70vh;
-  width: 100vh;
-}
-.form-input-group {
-  margin-bottom: 1rem;
-}
-label {
-  margin-right: 0.5rem;
-}
-form{
-  background-color: #B1ABED;
-  border-radius: 50px;
-  padding: 30px;
-}
-img {
-  height: 650px;
-  width: 1100px;
-  margin-top: 50px;
-  margin-left: 525px;
-  padding: 10px;
-
+.register-img{
+  background-image: url("https://drive.google.com/uc?id=1mQanX__aTknypqfjH8NEsl1tlVCim7U-");
   
+    
+    /* Control the height of the image */
+  height: 100%;
+  width: 100%;
 
+  /* Center and scale the image nicely */
+  background-position: center bottom;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  position: static;
+}
+
+/* Add styles to the form container */
+.register-form {
+  position: absolute;  
+  right: 11rem;
+  /* top: 5rem; */
+  margin: 20px 20px;
+  max-width: 300px;
+  padding: 10px 10px;
+  background-color:transparent;
+}
+/* Full-width input fields */
+  input[type=text], input[type=password] {
+  
+  width: 80%;
+  padding: 10px;
+  margin: 5px 0 10px 0;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type=text]:focus, input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+form router-link {
+  color: #66cef7;
+}
+label
+ {
+  color: #b1a1ed;
+  text-transform: capitalize;
+  font-family: cursive;
+  font-weight: normal;
+}
+
+/* Set a style for the submit button */
+button {
+  background-color: #04AA6D;
+  font-family: cursive;
+  font-weight: 2;  
+  font-size: 100%;
+  color: white;
+  margin: 5px;
+  padding: 10px;  
+  border: none;
+  cursor: pointer;
+  width: 88%;
+  opacity: 5;
+}
+
+button:hover {
+  opacity: 1;
+}
+h3 {
+  background-color: transparent;
+  margin: 1px;
+  padding: 2px;
+  border: none;
+  color: #66cef7;
+}
+
+/* Media queries for responsiveness */
+@media (max-width: 768px) {
+  .register-form {
+    max-width: 100%;
+    margin: 20px;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    left: 0;
+  }
+  input[type=text],
+  input[type=password] {
+    width: 100%;
+  }
+
+  button {
+    width: 100%;
+  }
 }
 
 </style>
